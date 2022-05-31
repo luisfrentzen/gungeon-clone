d6c84eed37ae83869de9cdf9f5998fe97c87490a
luisfrentzen <luisfrentzen@gmail.com>
2022-05-31 11:36:32 +0700

    resolved pull conflict

--- src/application/view/GameSceneView.java
+++ src/application/view/GameSceneView.java
@@ -1,24 +1,25 @@
 package application.view;
 
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 
 public class GameSceneView extends SceneView{
 
-	private Label menuLabel;
+	private Canvas canvas;
 	
 	@Override
 	protected void initComponents() {
 		// TODO Auto-generated method stub
 		this.root = new StackPane();
-		this.menuLabel = new Label("Game");
+		this.canvas = new Canvas();
 	}
 
 	@Override
 	protected void addComponents() {
 		// TODO Auto-generated method stub
-		this.root.getChildren().add(menuLabel);
+		this.root.getChildren().add(this.canvas);
 	}
 
 	@Override
